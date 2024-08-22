128f430b45748d5662443e4875e77fa845cc2e4b
arthur1573 <37476653+arthur1573@users.noreply.github.com>
2024-08-22 12:23:58 +0800

    modified

--- one-click-install-ai.sh
+++ one-click-install-ai.sh
@@ -21,5 +21,8 @@ ollama run llama3
 
 
 
+
 # docker run website
-docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+# remove: --name open-webui
+# remove: --restart always
+docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434  ghcr.io/open-webui/open-webui:main
