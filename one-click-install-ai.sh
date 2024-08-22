67cd46a40a87e922f00bc3c71bf4ae5a5104b906
arthur1573 <37476653+arthur1573@users.noreply.github.com>
2024-08-22 11:37:13 +0800

    new file

---
+++ one-click-install-ai.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "one click install ai"
+echo "for github's codespace"
+
+
+
+
+# download ollama
+curl -fsSL https://ollama.com/install.sh
+
+
+
+# create a ollama server
+ollama serve
+
+# start the software
+ollama run llama3
+
+
+
+
+# docker run website
+docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
